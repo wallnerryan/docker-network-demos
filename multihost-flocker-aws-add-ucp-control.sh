ca7e1b88210bac87624183b276f6b20ac32bf57e
Ryan Wallner <ryan.wallner@clusterhq.com>
2016-06-05 16:40:16 -0400

    fix some bugs during ucp instatiation

--- multihost-flocker-aws-add-ucp-control.sh
+++ multihost-flocker-aws-add-ucp-control.sh
@@ -9,36 +9,41 @@ then
     exit 1
 fi
 
+group_name=${MY_SEC_GROUP_NAME:="docker-networking"}
 my_ip="$(wget -q -O- http://icanhazip.com)"
 
 group_id="$(aws ec2 describe-security-groups --filters Name=group-name,Values=${group_name} --query 'SecurityGroups[*].{Name:GroupId}' | python aws_get_group_id.py)"
-aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 443 --cidr ${my_ip}/32
-aws ec2 authorize-security-group-egress --group-id ${group_id} --protocol tcp --port 443 --cidr ${my_ip}/32
-aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 2376 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 2375 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol udp --port 4789 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-egress --group-id ${group_id} --protocol udp --port 4789 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 7946 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-egress --group-id ${group_id} --protocol tcp --port 7946 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol udp --port 7946 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-egress --group-id ${group_id} --protocol udp --port 7946 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12376 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12379 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12380 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12381 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12382 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12383 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12384 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12385 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12386 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 443 --cidr ${my_ip}/32 || true
+aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 443 --cidr 0.0.0.0/0 || true
+aws ec2 authorize-security-group-egress --group-id ${group_id} --protocol tcp --port 443 --cidr ${my_ip}/32 || true
+aws ec2 authorize-security-group-egress --group-id ${group_id} --protocol tcp --port 443 --cidr 0.0.0.0/0 || true
+aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 2375 --cidr 0.0.0.0/0 || true
+#alternative swarm port since docker already uses it.
+aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 23766 --cidr 0.0.0.0/0 || true
+aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol udp --port 4789 --cidr 0.0.0.0/0 || true
+aws ec2 authorize-security-group-egress --group-id ${group_id} --protocol udp --port 4789 --cidr 0.0.0.0/0 || true
+aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 7946 --cidr 0.0.0.0/0 || true
+aws ec2 authorize-security-group-egress --group-id ${group_id} --protocol tcp --port 7946 --cidr 0.0.0.0/0 || true
+aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol udp --port 7946 --cidr 0.0.0.0/0 || true
+aws ec2 authorize-security-group-egress --group-id ${group_id} --protocol udp --port 7946 --cidr 0.0.0.0/0 || true
+aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12376 --cidr 0.0.0.0/0 || true
+aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12379 --cidr 0.0.0.0/0 || true
+aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12380 --cidr 0.0.0.0/0 || true
+aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12381 --cidr 0.0.0.0/0 || true
+aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12382 --cidr 0.0.0.0/0 || true
+aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12383 --cidr 0.0.0.0/0 || true
+aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12384 --cidr 0.0.0.0/0 || true
+aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12385 --cidr 0.0.0.0/0 || true
+aws ec2 authorize-security-group-ingress --group-id ${group_id} --protocol tcp --port 12386 --cidr 0.0.0.0/0 || true
 
 
 docker $(docker-machine config mha-aws-consul) run -d \
-   --rm --name ucp \
+   --name ucp \
    -v /var/run/docker.sock:/var/run/docker.sock \
    docker/ucp install \
    --fresh-install \
+   --swarm-port 23766 \
    --host-address=$(docker-machine ip mha-aws-consul) \
-   --san $(docker-machine ip mha-aws-consul)"
+   --san $(docker-machine ip mha-aws-consul)
 
 
--- multihost-flocker-aws-add-ucp-nodes.sh
+++ multihost-flocker-aws-add-ucp-nodes.sh
@@ -5,7 +5,7 @@ set -e
 #### Set Up Environment
 
 if [ -z $UCP_FINGERPRINT ]; then
-    echo "Please supply your AWS_ACCESS_KEY_ID"
+    echo "Please supply your UCP_FINGERPRINT"
     exit 1
 fi
 
@@ -13,18 +13,19 @@ export CLUSTER_SIZE=${MY_CLUSTER_SIZE:=3}
 
 # assumes you have run `multihost-flocker-aws-add-ucp-control.sh` for controller
 ((AGENTS = ${CLUSTER_SIZE} - 1))
-for i in `seq 1 ${AGENTS}`;
+for i in `seq 0 ${AGENTS}`;
 do
    docker $(docker-machine config mha-aws-demo${i}) run -d \
       --name ucp \
       -v /var/run/docker.sock:/var/run/docker.sock \
       -e UCP_ADMIN_USER=admin -e UCP_ADMIN_PASSWORD=orca \
       docker/ucp join \
+      --swarm-port 23766 \
       --fresh-install \
       --san $(docker-machine ip mha-aws-demo${i}) \
       --host-address $(docker-machine ip mha-aws-demo${i}) \
-      --url $(docker-machine ip mha-aws-consul) \
-      --fingerprint $(UCP_FINGERPRINT)"
+      --url https://$(docker-machine ip mha-aws-consul) \
+      --fingerprint ${UCP_FINGERPRINT}
 done
 
 echo "Done!"
